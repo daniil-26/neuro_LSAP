ff793804b66d547a8401480909a884ff8ad060a2
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-05-31 13:22:51 +0300

    Update solve.py

--- solve.py
+++ solve.py
@@ -4,62 +4,45 @@ from hungarian_algorithm import algorithm
 random.seed()
 
 
-def hungarian(e):
-    n = len(e)
-    g = {str(i) + '_': {str(j): e[i][j] for j in range(n)} for i in range(n)}
-    s = algorithm.find_matching(g, matching_type='max', return_type='list')
-    s = [x[0] for x in s]
-    s = [[1 if (str(i) + '_', str(j)) in s else 0 for j in range(n)] for i in range(n)]
-    return s
+def hungarian(condition):
+    dimension = len(condition)
+    condition = {str(i) + '_': {str(j): condition[i][j] for j in range(dimension)} for i in range(dimension)}
+    solution = algorithm.find_matching(condition, matching_type='max', return_type='list')
+    solution = [x[0] for x in solution]
+    solution = [[1 if (str(i) + '_', str(j)) in solution else 0 for j in range(dimension)] for i in range(dimension)]
+    return solution
 
 
-def greedy(expenses):
-    size = len(expenses)
-    solution = [[0 for _ in range(size)] for _ in range(size)]
-    for i in range(size):
-        j = expenses[i].index(max(expenses[i]))
+def greedy(condition):
+    condition_copy = condition.copy()
+    dimension = len(condition_copy)
+    solution = [[0 for _ in range(dimension)] for _ in range(dimension)]
+    for i in range(dimension):
+        j = condition_copy[i].index(max(condition_copy[i]))
         solution[i][j] = 1
-        for k in range(i + 1, size):
-            expenses[k][j] = -1
+        for k in range(i + 1, dimension):
+            condition_copy[k][j] = -1
     return solution
 
 
-def random_expenses(size):
-    return [[random.randint(0, 100) for _ in range(size)] for _ in range(size)]
+def random_method(condition):
+    dimension = len(condition)
+    return random_solution(dimension)
 
 
-def collision_avoidance(expenses, solution):
-    n = min(len(expenses), len(solution))
-    for i in range(n):
-        if [solution[j][i] for j in range(n)].count(1) > 1:
-            max_parameter = max([expenses[j][i] for j in range(n) if solution[j][i]])
-            for j in range(n):
-                if expenses[j][i] < max_parameter:
-                    solution[j][i] = 0
-    for i in range(n):
-        if 1 not in solution[i]:
-            max_parameter = max(expenses[i][j] for j in range(n) if 1 not in [solution[k][j] for k in range(n)])
-            for j in range(n):
-                if expenses[i][j] == max_parameter and 1 not in [solution[k][j] for k in range(n)]:
-                    solution[i][j] = 1
-                    break
-    return solution
+def random_condition(dimension):
+    return [[random.random() for _ in range(dimension)] for _ in range(dimension)]
 
 
-def solution_transformation(solution):
-    x = solution.index(max(solution))
-    return [1 if i == x else 0 for i in range(len(solution))]
-
-
-def random_solution(size):
-    x = list(range(0, size))
+def random_solution(dimension):
+    x = list(range(0, dimension))
     random.shuffle(x)
-    return [[1 if j == x[i] else 0 for j in range(size)] for i in range(size)]
+    return [[1 if j == x[i] else 0 for j in range(dimension)] for i in range(dimension)]
 
 
-def objective_function(expenses, solution):
-    n = min(len(expenses), len(solution))
-    return sum(sum([[expenses[i][j] * solution[i][j] for j in range(n)] for i in range(n)], []))
+def objective_function(condition, solution):
+    dimension = len(condition)
+    return sum(sum([[condition[i][j] * solution[i][j] for j in range(dimension)] for i in range(dimension)], []))
 
 
 def extension(array, x):
